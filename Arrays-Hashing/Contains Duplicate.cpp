b6aae69692e8e3f2f18edb98afe41e15e13be062
Bhavya Jain <misticmellow321@gmail.com>
2026-08-14 22:08:16 +0530

    Update Contains Duplicate.cpp

--- Arrays-Hashing/Contains Duplicate.cpp
+++ Arrays-Hashing/Contains Duplicate.cpp
@@ -1,1 +1,19 @@
-
+#include <unordered_set>
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_set <int> values;
+        for (int i =0;i<n;i++){
+            if (values.count(nums[i])){
+                return true;
+                
+            }
+            else{
+                values.insert(nums[i]);
+                
+            }
+        }
+        return false;
+    }
+};
